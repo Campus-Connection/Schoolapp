287cb59bd117fa6a1ba05a5725eab5e5c13246b6
ryaotuix <ryaotuix@naver.com>
2023-02-11 12:34:45 -0500

    .

---
+++ Student/student.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Student
+{
+    // Aspects all private
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private String[] classList;
+
+    // Constructors
+    public Student(String name, String username, String password, String email, String[] classList)
+    {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.classList = classList;
+    }
+
+    // Change values
+    public
+
+}
